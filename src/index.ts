321bd8449e4dceceec0a565118fec3a61ec37a91
Pedro Obi <obipedro121@gmail.com>
2023-01-13 23:05:02 +0000

    configured mongodb using mongoose to access it cluster on the mongodb atlas

--- src/index.ts
+++ src/index.ts
@@ -1,17 +1,25 @@
 import express, { Request, Response } from "express";
+import mongoose from "mongoose";
+import 'dotenv/config'
 const app = express();
 
-// Parse JSON 
-app.use(express.json());
 
-app.get("/", (req: Request, res: Response) => {
-    res.json({
-        message: "Welcome to the new dispensation"
-    })
-})
+const start = async () => {
+    if (!process.env.MONGO_URI) {
+        throw new Error('URI not found')
+        }
+        
+    try {
+        await mongoose.connect(process.env.MONGO_URI);
+        console.log('Connected to mongodb  database')
+        app.listen(3000, ()=>{
+            console.log('Listening on port 3000')
+        })
+        
+    } catch (error) {
+        console.log(error)
+    }
+}
+start()
 
-const port = process.env.PORT || 3000;
 
-app.listen(port, () => {
-    console.log(`Server is running on port ${port}`);
-})
